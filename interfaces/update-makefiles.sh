sYYLG-TEMP/android_device_xiaomi_grus

grus: Import Xiaomi HAL definitions
Change-Id: If3642b547a09d84cb8cc6101960a90da2b8579e1

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.xiaomi:device/xiaomi/cepheus/interfaces"

Depau/device_xiaomi_pyxis-1

Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.xiaomi:device/xiaomi/grus/interfaces"