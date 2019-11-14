34750c8b64b4bfe7cfd7f4149ca1049fa5fcc885
Nanda Oktavera <codeharuka.yusa@gmail.com>
2019-11-15 04:09:43 +0900

    grus: update device-specific props
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>

--- device-props.mk
+++ device-props.mk
@@ -1,5 +1,7 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.cfa.thres.front=0 \
+    persist.camera.cfa.thres.rear=200 \
     persist.camera.sat.fallback.dist=28 \
     persist.camera.sat.fallback.dist.d=5 \
     persist.camera.sat.fallback.luxindex=230 \
@@ -11,15 +13,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.display.type=oled \
     ro.displayfeature.histogram.enable=true \
-    ro.eyecare.brightness.threshold=11 \
     ro.eyecare.brightness.level=5 \
-    ro.hist.brightness.threshold=7
+    ro.eyecare.brightness.threshold=11 \
+    ro.hist.brightness.threshold=7 \
+    sys.displayfeature_hidl=true \
+    sys.displayfeature.hbm.enable=true
+
+# FOD
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    persist.vendor.sys.fp.fod.location.X_Y=448,1938 \
+    persist.vendor.sys.fp.fod.size.width_height=185,185 \
+    ro.hardware.fp.fod=true
 
 # NFC
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.hardware.nfc_nci=nqx.default
     ro.nfc.port=I2C
 
-# Radio
+# SSR
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.radio.uicc_se_enabled=true
+    persist.vendor.ssr.enable_ramdumps=0 \
+    persist.vendor.ssr.restart_level=ALL_ENABLE
